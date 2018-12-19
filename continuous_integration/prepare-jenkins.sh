27d4960262d986edc9dc8c33ea3c7dc754da0338
Fadri Furrer <ffurrer@gmail.com>
2018-12-19 13:12:56 +0100

    added prepare jenkins script

---
+++ continuous_integration/prepare-jenkins.sh
@@ -0,0 +1,10 @@
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+  echo "**** Installing additional necessary packages. ****"
+  sudo apt-get install -y libbullet-dev
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  brew install bullet
+else
+  echo "Platform $unamestr is not supported!"
+  exit -1
+fi
